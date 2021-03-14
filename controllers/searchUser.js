5385336c06508ca9e406e7213f07431a55d56147
Ashish <ashish.bec1234@gmail.com>
2021-03-14 12:34:31 +0530

    commit

--- controllers/searchUser.js
+++ controllers/searchUser.js
@@ -1,29 +1,23 @@
 const User = require('../models/users');
 const APPECTATION = require('../config/constants')
-const UserMethod = new User();
-require('dotenv').config(); 
-const jwt = require('jsonwebtoken');
 
 
 const searchUser = async (req,res)=>{
 
     try {
         // const validUser = req.UserData
-        let updateDetails = {
-            firstName:req.body.firstName,
-            lastName:req.body.lastName,
-            phone:req.body.phone,
-            email:req.body.email
-        }
+      let {firstName,lastName,phone,email} = req.body
+        
         let searchedUser = await User.find(
         {$or:[
-         { firstName:{"$regex": `.*${updateDetails.firstName}.*`,$options:'i'}},
-          {lastName:{"$regex": `.*${updateDetails.lastName}.*`,$options:'i'}},
-          {phone:{"$regex": `.*${updateDetails.phone}.*`,$options:'i'}},
-          {email:{"$regex": `.*${updateDetails.email}.*`,$options:'i'}}
+         { firstName:{"$regex": `.*${firstName}.*`,$options:'i'}},
+          {lastName:{"$regex": `.*${lastName}.*`,$options:'i'}},
+          {phone:{"$regex": `.*${phone}.*`,$options:'i'}},
+          {email:{"$regex": `.*${email}.*`,$options:'i'}}
         ]},{_id:1, firstName:1,lastName:1,email:1,phone:1 });
 
         res.json({Status:200,"Response":searchedUser});
+
         } catch (error) {
         res.json({RESPONSE: error.message})
     }
@@ -37,7 +31,9 @@ const Pagination = async(req,res)=>{
 
         const searchUserByPagination = await User.find({},{_id:1, firstName:1,lastName:1,email:1,phone:1 }).skip((pageQuery-1)*limitQuery).limit(limitQuery);
         if(!searchUserByPagination) throw new Error (APPECTATION.STATUSMESSAGE.YOU_HAVE_REACHED_MAX_LIMIT)
+       
         res.json({Status:200,"Response":searchUserByPagination});
+
     } catch (error) {
       res.json({RESPONSE: error.message})
     }
